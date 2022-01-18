4407b90914c5b1639e239ce7d02d71943a367798
DragomirSt <drago89@abv.bg>
2022-01-18 04:48:45 -0800

    Create GeoPosition.js
    implement functionality to ask the user wheather or not he agrees to give his geo location, so the app can show him the weather at the moment

---
+++ src/components/geoPosition/GeoPosition.js
@@ -0,0 +1,39 @@
+
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { DayContext } from '../../contexts/DayContext';
+
+import { geoLocation } from '../../weather-forecast/weater';
+
+const GeoPosition = () => {
+
+    const { setCityKey } = useContext(DayContext);
+    const navigate = useNavigate();
+
+    let long;
+    let lat;
+    window.addEventListener('load', () => {
+
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(positon => {
+
+                long = positon.coords.longitude;
+                lat = positon.coords.latitude;
+
+                geoLocation(lat, long)
+                    .then(res => {
+
+                        setCityKey({ key: res.Key, cityName: res.LocalizedName });
+                        navigate('/today');
+                    })
+
+            });
+        }
+    });
+
+    return (
+        null
+    );
+};
+
+export default GeoPosition;
