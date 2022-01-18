56c375e5d95060de6bd92204b606bbfd08646ec8
DragomirSt <drago89@abv.bg>
2022-01-18 04:52:33 -0800

    Create Navigation.js
    add navigation with functionality to search the location as well store the location KEY and the city Name using useContext, so the other components can have access to them

---
+++ src/components/navigation/Navigation.js
@@ -0,0 +1,44 @@
+
+import './Navigation.css';
+
+import { Link, useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
+
+import { DayContext } from '../../contexts/DayContext';
+import { getLocation } from '../../weather-forecast/weater';
+
+const Navigation = () => {
+
+    const navigate = useNavigate();
+    const { cityKey, setCityKey } = useContext(DayContext);
+
+    const searchLoaction = (e) => {
+        e.preventDefault();
+        let formData = new FormData(e.target);
+        let city = formData.get('city');
+
+        getLocation(city)
+            .then(res => {
+
+                setCityKey({ key: res.Key, cityName: res.LocalizedName });
+                navigate('/today');
+            });
+            e.target.reset();      
+    };
+  
+    return (
+        <div className="nav-flex">
+            <nav className="topnav">
+                <Link to="/today">Now</Link>
+                <Link to="/hourly">Hourly</Link>
+                <Link to="/fivedaysAhead">Daily</Link>
+                <form id="form-input" action="submit" onSubmit={searchLoaction}>
+                    <input type="text" name="city" placeholder="Search location..." />
+                </form>
+                <div>weather in: <h3>{cityKey.cityName}</h3></div>
+            </nav>
+        </div>
+    );
+};
+
+export default Navigation;
