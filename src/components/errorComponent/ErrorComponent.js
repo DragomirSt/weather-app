1af3bd7c83ba7e88d5146302e739f01cf2720f1d
DragomirSt <drago89@abv.bg>
2022-01-18 04:42:58 -0800

    Create ErrorComponent
    add error component so if someone click on the navigation before entering location in the input form the application won't crush

---
+++ src/components/errorComponent/ErrorComponent.js
@@ -0,0 +1,13 @@
+
+import './ErrorComponent.css';
+
+const ErrorComponent = () => {
+
+    return (
+        <div className='error-div'>
+            <h2 className='error-text'>Please enter your location ...</h2>
+        </div>
+    );
+};
+
+export default ErrorComponent;
