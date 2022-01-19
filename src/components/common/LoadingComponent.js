7a7c0f80bd5c8a3d58f38835174aaba6e74025b8
DragomirSt <drago89@abv.bg>
2022-01-18 22:04:22 -0800

    Create LoadingComponent
    added a loading component so the user can see that his forecast is loading

---
+++ src/components/common/LoadingComponent.js
@@ -0,0 +1,13 @@
+
+import './LoadingComponent.css';
+
+const LoadingComponent = () => {
+
+    return(
+        <div className='loading'>
+           <h2>Geting your foreceast ...</h2>
+        </div>
+    );
+};
+
+export default LoadingComponent;
