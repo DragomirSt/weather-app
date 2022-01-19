42f696fdbac75b08bc31ee45d434cebc985c83d9
DragomirSt <drago89@abv.bg>
2022-01-19 08:17:02 -0800

    Create ErrorComponent.js
    made a component which will show if something goes wrong

---
+++ src/components/common/ErrorComponent.js
@@ -0,0 +1,12 @@
+
+import './Common.css'
+const ErrorComponent = () => {
+
+    return (
+        <div className='common-div'>
+            <h2>Something went wrong :(</h2>
+        </div>
+    );
+};
+
+export default ErrorComponent;
