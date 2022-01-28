c285687439361cc1ab3689c4f0bbcd442dd95ba7
DragomirSt <drago89@abv.bg>
2022-01-27 23:42:14 -0800

    Update Navigation.js
    add a link to location when the app started, so if the user allow his location, the weather will be displayed

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -11,42 +11,40 @@ const Navigation = () => {
 
     const navigate = useNavigate();
     const { cityKey, setCityKey } = useContext(DayContext);
-   
-    const useSearchLoaction = (e) => {
+
+    const searchLocation = (e) => {
         e.preventDefault();
         let formData = new FormData(e.target);
         let city = formData.get('city');
 
         getLocation(city)
             .then(res => {
-           
+
                 setCityKey({ key: res.Key, cityName: res.LocalizedName });
                 navigate('/today');
             })
         e.target.reset();
     };
-  
-    return (
-        
-            <nav className="topnav">
-                {cityKey.key ?
-                    <>
-                        <Link to="/today">Now</Link>
-                        <Link to="/hourly">Hourly</Link>
-                        <Link to="/daily">Daily</Link>
-                    </>
-                    : <></>}
-                <form className="form-input" action="submit" onSubmit={useSearchLoaction}>
-                    <input type="text" name="city" placeholder="Enter your location..." />
-                </form>
-                {cityKey.cityName ?
-                    <div className='city'>
-                        weather in: {cityKey.cityName}
-                    </div>
 
-                    : <></>}
-            </nav>
-        
+    return (
+
+        <nav className="topnav">
+            {cityKey.key ?
+                <>
+                    <Link to="/today">Now</Link>
+                    <Link to="/hourly">Hourly</Link>
+                    <Link to="/daily">Daily</Link>
+                </>
+                : <Link to="/location">location</Link>}
+            <form className="form-input" action="submit" onSubmit={searchLocation}>
+                <input type="text" name="city" placeholder="Enter your location..." />
+            </form>
+            {cityKey.cityName ?
+                <div className='city'>
+                    weather in: {cityKey.cityName}
+                </div>
+                : null}
+        </nav>
     );
 };
 
