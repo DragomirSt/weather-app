5e1c29995878890ccc1e9c68f03bacc09044fb53
DragomirSt <drago89@abv.bg>
2022-02-01 00:43:01 -0800

    removed the custom hook

--- src/hooks/errorHook.js
+++
@@ -1,9 +0,0 @@
-
-import { useContext } from 'react';
-import { ErrorContext } from '../contexts/ErrorContext';
-
-const useErrorHandling = () => {
-    return useContext(ErrorContext);
-}
-
-export default useErrorHandling;
