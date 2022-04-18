da73d1579472a1c129d67b0f06619f85180910a6
DragomirSt <drago89@abv.bg>
2022-04-18 03:25:03 -0700

    Update Today.js
    change the context and make an error handling in case of a wrong city name input

--- src/components/today/Today.js
+++ src/components/today/Today.js
@@ -5,48 +5,62 @@ import { useState, useEffect, useContext } from 'react';
 import { Link } from 'react-router-dom';
 
 import { currentWeather } from "../../weather-forecast/weather";
-import { DayContext } from '../../contexts/DayContext';
+import { CityContext } from '../../contexts/CityContext';
 
 import LoadingComponent from '../common/LoadingComponent';
+import FetchError from '../common/FetchError';
 
 const Today = () => {
     const [weather, setWeather] = useState([]);
-    const { cityKey } = useContext(DayContext);
+    const { city } = useContext(CityContext);
 
     const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
 
     useEffect(() => {
         const controller = new AbortController();
         const signal = controller.signal;
         setLoading(true);
 
-        currentWeather(cityKey.key, {
+        currentWeather(city, {
             signal: signal
         })
             .then(res => {
                 const weatherData = [res];
                 setWeather(weatherData);
             })
+            .catch(error => {
+                setError(true);
+                setErrorMessage(error);
+            })
             .finally(() => {
                 setLoading(false);
             })
         return () => {
+            setError(false);
             controller.abort();
         };
-    }, [cityKey]);
 
+    }, [city]);
+
+    if (error) {
+        return <>
+            <FetchError props={errorMessage} />
+        </>
+    }
     if (loading) {
         return <>
             <LoadingComponent />
         </>
     };
-  
+
     return (
         <div>
-            {!!weather && weather.map(x => (
-                <div className="today-div-forecast">
+            {weather && weather.map(x => (
+                <div className="today-div-forecast" key={city}>
                     <div className='city-name'>
-                        {cityKey.key}
+                        {city}
                     </div>
                     <div className='city-temps'>
                         {x.current.feelslike_c} *C
