ec04f2a0a74b616c6c90462f192434483947f195
DragomirSt <drago89@abv.bg>
2022-04-18 03:19:11 -0700

    Crate FetchError.js
    add new component to display error if the string of the input filed don't match any result

---
+++ src/components/common/FetchError.js
@@ -0,0 +1,13 @@
+
+import './Common.css';
+
+const FetchError = ({props}) => {
+
+    return (
+        <div className='common-div'>
+            <h1>{props}</h1>
+        </div>
+    );
+};
+
+export default FetchError;
