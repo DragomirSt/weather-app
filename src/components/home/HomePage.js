48900bd19f4bafaf5f286bdc4c04f1637e158445
DragomirSt <drago89@abv.bg>
2022-04-18 03:19:44 -0700

    add HomePage.js

---
+++ src/components/home/HomePage.js
@@ -0,0 +1,14 @@
+
+import './HomePage.css'
+
+const HomePage = () => {
+
+    return (
+        <div className='home-page'>
+            <h1>Get Your Forecast</h1>
+            <h2>View hourly and ahead weather</h2>
+        </div>
+    );
+};
+
+export default HomePage;
