19deb73d6cab6a8b034cc947c74619cc44e62c4e
DragomirSt <drago89@abv.bg>
2022-05-03 02:09:54 -0700

    add NavLink to active links styling

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,22 +1,28 @@
 
 import './Navigation.css';
 
-import { Link, useNavigate } from 'react-router-dom';
-import { useContext } from 'react';
+import { NavLink, Link, useNavigate } from 'react-router-dom';
 
+import { useContext } from 'react';
 import { CityContext } from '../../contexts/CityContext';
 
 const Navigation = () => {
-
     const navigate = useNavigate();
     const { city, setCityName } = useContext(CityContext);
 
+    let activeStyle = {
+        'background-color': '#161618d0',
+        'border-radius': '15px',
+    };
+
     const searchLocation = (e) => {
         e.preventDefault();
-        const formData = new FormData(e.target);
-        const city = formData.get('city')
 
-        setCityName(city);
+        const formData = new FormData(e.target);
+        let cityName = formData.get('city').trim();
+        cityName = cityName.charAt(0).toUpperCase() + cityName.slice(1);
+
+        setCityName(cityName);
         e.target.reset();
         navigate('/today');
     };
@@ -25,9 +31,17 @@ const Navigation = () => {
         <nav className="topnav">
             {city ?
                 <>
-                    <Link to="/today">Now</Link>
-                    <Link to="/hourly">Hourly</Link>
-                    <Link to="/daily">Daily</Link>
+                    <NavLink to="/today" style={({ isActive }) =>
+                        isActive ? activeStyle : undefined
+                    }>Now</NavLink>
+                    <NavLink to="/hourly"
+                        style={({ isActive }) =>
+                            isActive ? activeStyle : undefined
+                        }>Hourly</NavLink>
+                    <NavLink to="/daily"
+                        style={({ isActive }) =>
+                            isActive ? activeStyle : undefined
+                        }>Daily</NavLink>
                 </>
                 : <Link to="/location">location</Link>}
             <form className='form' method='POST' type="text" onSubmit={searchLocation}>
